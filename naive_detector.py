809c55dae3ae129482f7543b81ccc690d8a9dee2
penolove <chinghua.yang@appier.com>
2019-03-04 22:53:08 +0800

    update requirement and detector

--- naive_detector.py
+++ naive_detector.py
@@ -2,6 +2,7 @@ import arrow
 
 from eyewitness.image_id import ImageId
 from eyewitness.image_utils import ImageHandler, Image
+from bistiming import SimpleTimer
 
 from cv2_detector import CascadeClassifierPersonWrapper, HogPersonDetectorWrapper, MobileNetWrapper
 
@@ -19,11 +20,14 @@ def get_person_detector(model):
 
 
 if __name__ == '__main__':
-    object_detector = get_person_detector('MobileNet')
+    model_name = 'MobileNet'
+    with SimpleTimer("Loading model %s" % model_name):
+        object_detector = get_person_detector(model_name)
     raw_image_path = 'demo/test_image.jpg'
     image_id = ImageId(channel='demo', timestamp=arrow.now().timestamp, file_format='jpg')
     image_obj = Image(image_id, raw_image_path=raw_image_path)
-    detection_result = object_detector.detect(image_obj)
+    with SimpleTimer("Predicting image with classifier"):
+        detection_result = object_detector.detect(image_obj)
     print("detected %s objects" % len(detection_result.detected_objects))
     ImageHandler.draw_bbox(image_obj.pil_image_obj, detection_result.detected_objects)
     ImageHandler.save(image_obj.pil_image_obj, "detected_image/drawn_image.jpg")
